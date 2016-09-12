1b523e927e16280c91f5939cf81295a641a98b34
Bryant Oblad <bryant.oblad@gmail.com>
2016-09-12 16:39:07 -0600

    adding servo

--- led.py
+++ led.py
@@ -3,13 +3,20 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
 
+servo = GPIO.PWM(18, 50)
+servo.start(7.5)
 
-while True:
-    print "LED on"
-    GPIO.output(18,GPIO.HIGH)
-    time.sleep(1)
-    print "LED off"
-    GPIO.output(18,GPIO.LOW)
-    time.sleep(1)
+try:
+    while True:
+        servo.ChangeDutyCycle(7.5)
+        time.sleep(1)
+        servo.ChangeDutyCycle(12.5)
+        time.sleep(1)
+        servo.ChangeDutyCycle(2.5)
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    servo.stop()
+    GPIO.cleanup()
