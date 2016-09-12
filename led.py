5453516639d324e88ba7c91d95ad24d5e7a720de
Bryant Oblad <bryant.oblad@gmail.com>
2016-09-12 16:17:39 -0600

    adding while loop

--- led.py
+++ led.py
@@ -4,8 +4,11 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
-print "LED on"
-GPIO.output(18,GPIO.HIGH)
-time.sleep(1)
-print "LED off"
-GPIO.output(18,GPIO.LOW)
+
+
+while True:
+    print "LED on"
+    GPIO.output(18,GPIO.HIGH)
+    time.sleep(1)
+    print "LED off"
+    GPIO.output(18,GPIO.LOW)
