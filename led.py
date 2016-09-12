ce73b35b63fd678d8512bc44860d4f1da07be06a
Bryant Oblad <bryant.oblad@gmail.com>
2016-09-12 17:33:33 -0600

    adding button

--- led.py
+++ led.py
@@ -4,16 +4,29 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18, GPIO.OUT)
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 servo = GPIO.PWM(18, 50)
 servo.start(7.5)
 
+light_is_on = False
+
+def turn_light_on():
+    light_is_on = True
+    servo.ChangeDutyCycle(9.5)
+    time.sleep(1)
+
+def turn_light_off():
+    light_is_on = False
+    servo.ChangeDutyCycle(5.5)
+    time.sleep(1)
+
 try:
     while True:
-        servo.ChangeDutyCycle(9.5)
-        time.sleep(1)
-        servo.ChangeDutyCycle(5.5)
-        time.sleep(1)
+        if GPIO.input(15) == 1 and light_is_on == False:
+            turn_light_on()
+        elif GPIO.input(15) == 1 and light_is_on == True:
+            turn_light_off()
 
 except KeyboardInterrupt:
     servo.stop()
